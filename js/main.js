edadf2aa790fa7f9ef4df703fa6f27242f874012
ElenaGonzalez2000 <elena.ig000@gmail.com>
2022-10-10 19:50:04 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -10,57 +10,82 @@ class Producto {
 }
 
 //declaracion de productos
-let productoA = new Producto('Top Adele', 999, 50, 'top', 'primavera');
-let productoB = new Producto('Top Jazmin', 890, 45, 'top', 'primavera')
-let productoC = new Producto('Remera Ruth', 1200, 15, 'remera', 'primavera')
-let productoD = new Producto('Remera Caliz', 1100, 2, 'remera', 'invierno')
+let productoA = new Producto('Top Adelle', 999, 50, 'top', 'primavera');
+let productoB = new Producto('Top Jazmin', 890, 45, 'top', 'primavera');
+let productoC = new Producto('Remera Ruth', 1200, 15, 'remera', 'primavera');
+let productoD = new Producto('Remera Caliz', 1100, 2, 'remera', 'invierno');
 let productoE = new Producto('Jean Tiza', 3100, 30, 'jean', 'primavera');
 let productoF = new Producto('Jean Luna', 3500, 20, 'jean', 'primavera');
 let productoG = new Producto('Jean Lola', 2900, 0, 'jean', 'invierno')
 let productoH = new Producto('Saco Lila', 1800, 5, 'abrigo', 'invierno');
 let productoI = new Producto('Campera Puffer', 6500, 0, 'abrigo', 'invierno');
 
-const productos = [productoA,productoB,productoC,productoD,productoE,productoF,productoG,productoH, productoI];
-console.log(productos)
+// const productos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG, productoH, productoI];
+
+const productos = [productoA, productoC, productoE];
 
 //filtro para saber los productos con stock
-const conStock = productos.filter((producto) => producto.stock > 0 );
-console.log(conStock)
+const conStock = productos.filter((producto) => producto.stock > 0);
 
 //filtro temporada de primavera
 const temporadaPrimavera = productos.filter((producto) => producto.temporada == 'primavera');
-console.log(temporadaPrimavera)
 
 //filtro temporada invierno 
 const temporadaInvierno = productos.filter((producto) => producto.temporada == 'invierno');
-console.log(temporadaInvierno)
 
 //filtros para dividir los productos entre precios :
 
 const entre0y3mil = productos.filter((producto) => producto.precio > 0 && producto.precio <= 3000);
-console.log(entre0y3mil)
 
 const entre3mily5mil = productos.filter((producto) => producto.precio >= 3000 && producto.precio <= 5000);
-console.log(entre3mily5mil)
 
 const entre5mily10mil = productos.filter((producto) => producto.precio >= 5000 && producto.precio <= 10000);
-console.log(entre5mily10mil)
+
 
 //filtros por tipo de ropa:
 
-const topsYRemeras = productos.filter((producto) => producto.tipo == 'top'|| producto.tipo == 'remera');
-console.log(topsYRemeras)
+const topsYRemeras = productos.filter((producto) => producto.tipo == 'top' || producto.tipo == 'remera');
 
 const jeansYShorts = productos.filter((producto) => producto.tipo == 'jean' || producto.tipo == 'short');
-console.log(jeansYShorts)
 
 const abrigos = productos.filter((producto) => producto.tipo == 'abrigo');
-console.log(abrigos)
 
 
-//simulando carrito
-const carrito = []
-carrito.push(productoA.precio, productoB.precio, productoE.precio)
-console.log(carrito)
-const total = carrito.reduce ((acumulador, precio) => acumulador + precio, 0)
-console.log(total)
+
+
+
+//simulando compra:
+
+const mapProductos = productos.map((el) => el.nombre)
+const mostrar = mapProductos.join('\n')
+console.log(mostrar)
+
+let alCarrito = prompt('puede elegir entre estos productos: \n' + mostrar + '\nEscriba el nombre del producto que quiere comprar o escriba 0 para salir');
+switch (alCarrito) {
+    case 'top adelle':
+        let cuantosA = parseInt(prompt('por favor indique cuantos quiere'))
+        if (cuantosA < productoA.stock) {
+            console.log('el total de su compra seria de: $' + productoA.precio * cuantosA);
+        }else {
+            console.log('exedio el stock disponible, por favor vuelva a empezar')
+        }
+        break;
+    case 'remera ruth':
+        let cuantosC = parseInt(prompt('por favor indique cuantos quiere'))
+        if (cuantosC < productoC.stock) {
+            console.log('el total de su compra seria de: $' + productoC.precio * cuantosC);
+        }else {
+            console.log('exedio el stock disponible, por favor vuelva a empezar')
+        }
+        break;
+        case 'jean tiza':
+            let cuantosE = parseInt(prompt('por favor indique cuantos quiere'))
+            if (cuantosE < productoE.stock) {
+                console.log('el total de su compra seria de: $' + productoE.precio * cuantosE);
+            }else {
+                console.log('exedio el stock disponible, por favor vuelva a empezar')
+            }
+            break;
+    default:
+        break;
+}
