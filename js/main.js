a7ba46fbf3c86bb2eab1c8007504580a7266633a
ElenaGonzalez2000 <elena.ig000@gmail.com>
2022-10-15 13:32:42 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-let img1 = '../images/productos/top (1).webp'
-let img2 = '../images/productos/top (2).webp'
-let img3 = '../images/productos/top (3).webp'
-let img4 = '../images/productos/remera (2).webp'
-let img5 = '../images/productos/remera (1).webp'
-let img6 = '../images/productos/remera (3).webp'
-let img7 = '../images/productos/short.webp'
-let img8 = '../images/productos/jean (1).webp'
-let img9 = '../images/productos/jean (2).webp'
-let img10 = '../images/productos/abrigo (1).webp'
-let img11 = '../images/productos/abrigo (3).webp'
-let img12 = '../images/productos/abrigo (2).webp'
+const img1 = '../images/productos/top (1).webp'
+const img2 = '../images/productos/top (2).webp'
+const img3 = '../images/productos/top (3).webp'
+const img4 = '../images/productos/remera (2).webp'
+const img5 = '../images/productos/remera (1).webp'
+const img6 = '../images/productos/remera (3).webp'
+const img7 = '../images/productos/short.webp'
+const img8 = '../images/productos/jean (1).webp'
+const img9 = '../images/productos/jean (2).webp'
+const img10 = '../images/productos/abrigo (1).webp'
+const img11 = '../images/productos/abrigo (3).webp'
+const img12 = '../images/productos/abrigo (2).webp'
 
 //funcion para crear los productos
 class Producto {
@@ -39,66 +39,81 @@ let productoK = new Producto(11, 'Campera Puffer', 6500, 0, 'abrigo', 'invierno'
 let productoL = new Producto(12, 'Chaleco Puffer', 6200, 3, 'abrigo', 'invierno', img12);
 
 
+//variable que guarda los productos
 const productos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG, productoH, productoI, productoJ, productoK, productoL];
-console.log(productos)
 
-//filtro para saber los productos con stock
+
+//filtro para saber los productos con stock:
 const conStock = productos.filter((producto) => producto.stock > 0);
-
-//filtro temporada de primavera
+//filtro temporada de primavera:
 const temporadaPrimavera = productos.filter((producto) => producto.temporada == 'primavera');
-
-//filtro temporada invierno 
+//filtro temporada invierno:
 const temporadaInvierno = productos.filter((producto) => producto.temporada == 'invierno');
-
-//filtros para dividir los productos entre precios :
-
+//filtros para dividir los productos entre precios:
 const entre0y3mil = productos.filter((producto) => producto.precio > 0 && producto.precio <= 3000);
-
 const entre3mily5mil = productos.filter((producto) => producto.precio >= 3000 && producto.precio <= 5000);
-
 const entre5mily10mil = productos.filter((producto) => producto.precio >= 5000 && producto.precio <= 10000);
-
-
 //filtros por tipo de ropa:
-
 const topsYRemeras = productos.filter((producto) => producto.tipo == 'top' || producto.tipo == 'remera');
-
 const jeansYShorts = productos.filter((producto) => producto.tipo == 'jean' || producto.tipo == 'short');
-
 const abrigos = productos.filter((producto) => producto.tipo == 'abrigo');
 
 
 
-const contenedor = document.getElementById('contenedorTienda');
+let contenedor = document.getElementById('contenedorTienda');
+let cart = []
+
+
+function addProdToCart(event) {
+    cart.push(event.target.getAttribute('card-button'))
+    renderCart()
+}
 
 const renderProductos = (products, target) => {
     let acumulador = '';
-    products.map(product=>{
+    products.map(product => {
         acumulador += `
         <div class="card" id="card">
-        <div class="card-img">
-            <img src="${product.imgUrl}"" srcset="" alt="${product.nombre}"></div>
-        <div class="card-info">
-            <div class="card-name-precio">
-                <h3 class="fuente">${product.nombre}</h3>
-                <h4 class="fuente">$${product.precio}</h4>
+            <div class="card-img">
+                <img src="${product.imgUrl}"" srcset="" alt="${product.nombre}">
             </div>
-            <div class="card-button">
-                <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle add-card" viewBox="0 0 16 16">
+            <div class="card-info">
+                <div class="card-name-precio">
+                    <h3 class="fuente">${product.nombre}</h3>
+                    <h4 class="fuente">$${product.precio}</h4>
+                </div>
+                <button ref=${product.id} class="card-button"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"class="bi bi-plus-circle add-card" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5  0    0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+                </button>
+            </div>
+        </div>`
+    target.innerHTML = acumulador;
+    })
+    
+    let cardButton = document.getElementsByClassName('card-button');
+    cardButton.addEventListener('click', addProdToCart);
+}
+renderProductos(productos, contenedor);
+
+// carito   
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+<div class="card-button" id="mark" >
+                <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"class="bi bi-plus-circle add-card" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                 </svg></a>
-            </div>
-        </div>
-    </div>
-        `
-    })
-
-    target.innerHTML = acumulador;
-}
-
-renderProductos(productos, contenedor);
-
-
-// carito   
+            </div>*/
