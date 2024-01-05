aa4166fb0db7ad60035ab20f95444a7cf6395807
Elena <elena.ig000@gmail.com>
2024-01-04 23:42:22 -0300

    :art: update

--- js/main.js
+++ js/main.js
@@ -1,87 +1,205 @@
-const img1 = '../images/productos/top (1).webp'
-const img2 = '../images/productos/top (2).webp'
-const img3 = '../images/productos/top (3).webp'
-const img4 = '../images/productos/remera (2).webp'
-const img5 = '../images/productos/remera (1).webp'
-const img6 = '../images/productos/remera (3).webp'
-const img7 = '../images/productos/short.webp'
-const img8 = '../images/productos/jean (1).webp'
-const img9 = '../images/productos/jean (2).webp'
-const img10 = '../images/productos/abrigo (1).webp'
-const img11 = '../images/productos/abrigo (3).webp'
-const img12 = '../images/productos/abrigo (2).webp'
+const img1 = "../images/productos/top (1).webp";
+const img2 = "../images/productos/top (2).webp";
+const img3 = "../images/productos/top (3).webp";
+const img4 = "../images/productos/remera (2).webp";
+const img5 = "../images/productos/remera (1).webp";
+const img6 = "../images/productos/remera (3).webp";
+const img7 = "../images/productos/short.webp";
+const img8 = "../images/productos/jean (1).webp";
+const img9 = "../images/productos/jean (2).webp";
+const img10 = "../images/productos/abrigo (1).webp";
+const img11 = "../images/productos/abrigo (3).webp";
+const img12 = "../images/productos/abrigo (2).webp";
 
 //funcion para crear los productos
 class Producto {
-    constructor(id, nombre, precio, stock, tipo, temporada, img, cantidad) {
-        this.id = id
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.tipo = tipo;
-        this.temporada = temporada;
-        this.imgUrl = img
-        this.cantidad = cantidad
-    }
+  constructor(id, nombre, precio, stock, tipo, temporada, img, cantidad) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.tipo = tipo;
+    this.temporada = temporada;
+    this.imgUrl = img;
+    this.cantidad = cantidad;
+  }
 }
 
 //declaracion de productos
-let productoA = new Producto(1, 'Top Adelle', 999, 50, 'top', 'primavera', img1, 1);
-let productoB = new Producto(2, 'Top Jazmin', 890, 45, 'top', 'primavera', img2, 1);
-let productoC = new Producto(3, 'Top Ameli', 990, 48, 'top', 'primavera', img3, 1);
-let productoD = new Producto(4, 'Remera Dino', 1200, 25, 'remera', 'primavera', img4, 1);
-let productoE = new Producto(5, 'Remera Ruth', 1150, 30, 'remera', 'primavera', img5, 1);
-let productoF = new Producto(6, 'Remera Mar', 1100, 4, 'remera', 'invierno', img6, 1);
-let productoG = new Producto(7, 'Short Nute', 3100, 30, 'short', 'primavera', img7, 1);
-let productoH = new Producto(8, 'Jean Luna', 3500, 25, 'jean', 'primavera', img8, 1);
-let productoI = new Producto(9, 'Jean Lola', 2900, 2, 'jean', 'invierno', img9, 1)
-let productoJ = new Producto(10, 'Buzo Lila', 1800, 5, 'abrigo', 'invierno', img10, 1);
-let productoK = new Producto(11, 'Campera Puffer', 6500, 5, 'abrigo', 'invierno', img11, 1);
-let productoL = new Producto(12, 'Chaleco Puffer', 6200, 2, 'abrigo', 'invierno', img12, 1);
-
+let productoA = new Producto(
+  1,
+  "Top Frunce",
+  4500,
+  50,
+  "top",
+  "primavera",
+  img1,
+  1
+);
+let productoB = new Producto(
+  2,
+  "Top Morley",
+  3990,
+  45,
+  "top",
+  "primavera",
+  img2,
+  1
+);
+let productoC = new Producto(
+  3,
+  "Top White",
+  3000,
+  48,
+  "top",
+  "primavera",
+  img3,
+  1
+);
+let productoD = new Producto(
+  4,
+  "Remera Dino",
+  4200,
+  25,
+  "remera",
+  "primavera",
+  img4,
+  1
+);
+let productoE = new Producto(
+  5,
+  "Remera Bicolor",
+  3250,
+  30,
+  "remera",
+  "primavera",
+  img5,
+  1
+);
+let productoF = new Producto(
+  6,
+  "Remera Green",
+  3500,
+  4,
+  "remera",
+  "invierno",
+  img6,
+  1
+);
+let productoG = new Producto(
+  7,
+  "Short Pink",
+  3900,
+  30,
+  "short",
+  "primavera",
+  img7,
+  1
+);
+let productoH = new Producto(
+  8,
+  "Jean Mom",
+  5500,
+  25,
+  "jean",
+  "primavera",
+  img8,
+  1
+);
+let productoI = new Producto(
+  9,
+  "Jean Wide",
+  8900,
+  2,
+  "jean",
+  "invierno",
+  img9,
+  1
+);
+let productoJ = new Producto(
+  10,
+  "Buzo Oversize",
+  7500,
+  5,
+  "abrigo",
+  "invierno",
+  img10,
+  1
+);
+let productoK = new Producto(
+  11,
+  "Campera Puffer",
+  16500,
+  5,
+  "abrigo",
+  "invierno",
+  img11,
+  1
+);
+let productoL = new Producto(
+  12,
+  "Chaleco Puffer",
+  13200,
+  2,
+  "abrigo",
+  "invierno",
+  img12,
+  1
+);
 
 //variable que guarda los productos
-const productos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG, productoH, productoI, productoJ, productoK, productoL];
+const productos = [
+  productoA,
+  productoB,
+  productoC,
+  productoD,
+  productoE,
+  productoF,
+  productoG,
+  productoH,
+  productoI,
+  productoJ,
+  productoK,
+  productoL,
+];
 
+const contenedorTienda = document.getElementById("contenedor-tienda"); //llamo al contenedor de la tienda
+const contenedorCarrito = document.getElementById("cart-body"); //body del carrito
+const vaciarCarrito = document.getElementById("vaciarCart"); //boton vaciar carrito
+const contadorCarrito = document.getElementById("contadorCarrito"); //contador del carrito
+const totalCarrito = document.getElementById("precioTotal"); //calcular el total
 
-const contenedorTienda = document.getElementById('contenedor-tienda'); //llamo al contenedor de la tienda
-const contenedorCarrito = document.getElementById('cart-body')//body del carrito
-const vaciarCarrito = document.getElementById('vaciarCart')//boton vaciar carrito 
-const contadorCarrito = document.getElementById('contadorCarrito')//contador del carrito
-const totalCarrito = document.getElementById('precioTotal')//calcular el total
+let cart = [];
 
-let cart = []
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+    actualizarCarrito();
+  }
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('cart')) {
-        cart = JSON.parse(localStorage.getItem('cart'))
-        actualizarCarrito()
-    }
-})
+vaciarCarrito.addEventListener("click", () => {
+  cart.length = 0;
+  Toastify({
+    className: "toastify",
+    text: "Vaciaste el carrito",
+    duration: 1000,
+    close: false,
+    gravity: "top", 
+    position: "left", 
+    stopOnFocus: false, 
+    style: {
+      background: "rgb(255, 176, 231)",
+    },
+    onClick: function () {},
+  }).showToast();
+  actualizarCarrito();
+});
 
-vaciarCarrito.addEventListener('click', () => {
-    cart.length = 0
-    Toastify({
-        className: "toastify",
-        text: 'Vaciaste el carrito',
-        duration: 1000,
-        close: false,
-        gravity: "top", // `top` or `bottom`
-        position: "left", // `left`, `center` or `right`
-        stopOnFocus: false, // Prevents dismissing of toast on hover
-        style: {
-            background: "rgb(255, 176, 231)",
-        },
-        onClick: function () { } // Callback after click
-    }).showToast();
-    actualizarCarrito()
-})
-
-//cards de productos en la pagina tienda
 productos.forEach((product) => {
-    const div = document.createElement('div')
-    div.classList.add('card')
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
     <div class="card-img">
         <img src="${product.imgUrl}"" srcset="" alt="${product.nombre}">
     </div>
@@ -90,82 +208,79 @@ productos.forEach((product) => {
         <h3 class="fuente">${product.nombre}</h3>
         <h4 class="fuente">$${product.precio}</h4>
         </div>
-        <button ref='' id="agregar${product.id}" class="card-button"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"class="bi bi-plus-circle add-card" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5  0    0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
-        </button>
     </div>
-        `
-    contenedorTienda.appendChild(div)
-    const boton = document.getElementById(`agregar${product.id}`);
-    boton.addEventListener('click', () => {
-        agregarCarrito(product.id)
-    })
-})
+    <button ref='' id="agregar${product.id}" class="card-button">Agregar al carrito
+        </button>
+        `;
+  contenedorTienda.appendChild(div);
+  const boton = document.getElementById(`agregar${product.id}`);
+  boton.addEventListener("click", () => {
+    agregarCarrito(product.id);
+  });
+});
 
 // agregar al carrito
 const agregarCarrito = (prodId) => {
-    //cantidad del mismo elemento en el carrito
-    const existe = cart.some(product => product.id === prodId)
+    const existe = cart.find((product) => product.id === prodId);
+
     if (existe) {
-        const product = cart.map(product => {
-            if (product.id === prodId) {
-                product.cantidad++
-            }
-        })
+        existe.cantidad++;
     } else {
-        const item = productos.find((prod) => prod.id === prodId)
-        cart.push(item)
-        console.log(cart)
+        const item = productos.find((prod) => prod.id === prodId);
+        cart.push(item);
     }
+
     Toastify({
         className: "toastify",
-        text: 'Agregaste un producto al carrito',
+        text: "Agregaste un producto al carrito",
         duration: 1000,
         close: false,
-        gravity: "top", // `top` or `bottom`
-        position: "left", // `left`, `center` or `right`
-        stopOnFocus: false, // Prevents dismissing of toast on hover
+        gravity: "top",
+        position: "left",
+        stopOnFocus: false,
         style: {
             background: "rgb(255, 176, 231)",
         },
-        onClick: function () { } // Callback after click
+        onClick: function () { },
     }).showToast();
-    actualizarCarrito()
-}
+
+    actualizarCarrito();
+};
 
 
-// eliminar producto del carrito 
+// eliminar producto del carrito
 const eliminarDelCarrito = (prodId) => {
-    const item = productos.find((prod) => prod.id === prodId)
-    const indice = cart.indexOf(item)
-    cart.splice(indice, 1)
-    actualizarCarrito()
-    Toastify({
-        className: "toastify",
-        text: 'Eliminaste un producto',
-        duration: 1000,
-        close: false,
-        gravity: "top", // `top` or `bottom`
-        position: "left", // `left`, `center` or `right`
-        stopOnFocus: false, // Prevents dismissing of toast on hover
-        style: {
-            background: "rgb(255, 176, 231)",
-        },
-        onClick: function () { } // Callback after click
-    }).showToast();
-}
+    const index = cart.findIndex((item) => item.id === prodId);
 
+    if (index !== -1) {
+        cart.splice(index, 1);
+        actualizarCarrito();
+
+        Toastify({
+            className: "toastify",
+            text: 'Eliminaste un producto',
+            duration: 1000,
+            close: false,
+            gravity: "top",
+            position: "left",
+            stopOnFocus: false,
+            style: {
+                background: "rgb(255, 176, 231)",
+            },
+            onClick: function () { }
+        }).showToast();
+    }
+};
 
 
 // actualizar carrito
 const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = ''
-    localStorage.setItem('cart', JSON.stringify(cart))
-    cart.forEach((product) => {
-        const div = document.createElement('div')
-        div.classList.add('cart-card')
-        div.innerHTML = `
+  contenedorCarrito.innerHTML = "";
+  localStorage.setItem("cart", JSON.stringify(cart));
+  cart.forEach((product) => {
+    const div = document.createElement("div");
+    div.classList.add("cart-card", "border");
+    div.innerHTML = `
         <div class="cart-info-box">
             <img src="${product.imgUrl}"  alt="" srcset="">
             <div class="cart-info-product">
@@ -177,9 +292,12 @@ const actualizarCarrito = () => {
         <div class="cart-buttons">
             <button onclick="eliminarDelCarrito(${product.id})" class="cart-button-delate">eliminar</button>
         </div>
-        `
-        contenedorCarrito.appendChild(div)
-    })
-    contadorCarrito.innerText = cart.length
-    totalCarrito.innerText = cart.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
-}
+        `;
+    contenedorCarrito.appendChild(div);
+  });
+  contadorCarrito.innerText = cart.length;
+  totalCarrito.innerText = cart.reduce(
+    (acc, prod) => acc + prod.precio * prod.cantidad,
+    0
+  );
+};
